7c06f229ca01b16c1b59fb9a0a3607bf5ddc1178
Sebastian Pichelhofer <sp@apertus.org>
2016-10-28 20:30:32 +0200

    updated picture snap script

--- beta-scripts/picture_snap.sh
+++ beta-scripts/picture_snap.sh
@@ -6,6 +6,7 @@
 PARAMS=$1
 IMAGENAME=`date +%Y%m%d_%H%M%S`
 mkdir -p /opt/picture-snap/$IMAGENAME
+echo /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12
 
 # Stop HDMI live stream so it does not interfere with image snapping
 fil_reg 15 0
@@ -16,8 +17,13 @@ fil_reg 15 0
 #convert to DNG
 ./raw2dng /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12 --swap-lines 
 
+#raw develop preview
+dcraw -h /opt/picture-snap/$IMAGENAME/$IMAGENAME.DNG
+
+#conert preview to JPG
+convert /opt/picture-snap/$IMAGENAME/$IMAGENAME.ppm /opt/picture-snap/$IMAGENAME/$IMAGENAME.jpg
 
 # restart HDMI live stream
 fil_reg 15 0x01000100
 
-echo /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12
+echo "done!"
