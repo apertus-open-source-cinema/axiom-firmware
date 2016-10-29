118f91b15e7f98cf8ddd9b07fd4c5ee7238e56d9
Sebastian Pichelhofer <sp@apertus.org>
2016-10-29 20:16:02 +0200

    updated picture snap script

--- beta-scripts/picture_snap.sh
+++ beta-scripts/picture_snap.sh
@@ -8,22 +8,35 @@ IMAGENAME=`date +%Y%m%d_%H%M%S`
 mkdir -p /opt/picture-snap/$IMAGENAME
 echo /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12
 
+#TODO handle $PARAMS being empty
+
 # Stop HDMI live stream so it does not interfere with image snapping
 fil_reg 15 0
 
 #capture frame
 ./cmv_snap3 -e $PARAMS -2 -r -b > /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12
 
-#convert to DNG
-./raw2dng /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12 --swap-lines 
-
-#raw develop preview
-dcraw -h /opt/picture-snap/$IMAGENAME/$IMAGENAME.DNG
-
-#conert preview to JPG
-convert /opt/picture-snap/$IMAGENAME/$IMAGENAME.ppm /opt/picture-snap/$IMAGENAME/$IMAGENAME.jpg
+echo "raw12 written"
 
 # restart HDMI live stream
 fil_reg 15 0x01000100
 
-echo "all done - JPG written!"
+#convert to DNG
+./raw2dng /opt/picture-snap/$IMAGENAME/$IMAGENAME.raw12 --swap-lines 
+
+echo "DNG written"
+
+#raw develop preview
+dcraw -h /opt/picture-snap/$IMAGENAME/$IMAGENAME.DNG
+
+echo "PPM written"
+
+#conert preview to JPG
+convert /opt/picture-snap/$IMAGENAME/$IMAGENAME.ppm /opt/picture-snap/$IMAGENAME/$IMAGENAME.jpg
+
+echo "JPG written"
+
+md5sum /root/darkframe-x1.pgm >  /opt/picture-snap/$IMAGENAME/$IMAGENAME.meta
+
+
+echo "all done!"
