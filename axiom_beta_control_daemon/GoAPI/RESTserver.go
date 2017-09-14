dbadd63172f82605c7dbb540e7bbaac1dcc2b198
BAndiT1983 <b.andrej@cumir.de>
2017-09-15 00:26:22 +0200

    - Adjusted test GUI and Go server - Basic communication is implemented

--- axiom_beta_control_daemon/GoAPI/RESTserver.go
+++ axiom_beta_control_daemon/GoAPI/RESTserver.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -18,6 +20,9 @@ func (server *RESTserver) Init() {
 	router.HandleFunc("/", Index)
 	router.HandleFunc("/todos", TodoIndex)
 	router.HandleFunc("/todos/{todoId}", TodoShow)
+	router.HandleFunc("/settings", SettingsShowGET).Methods("GET")
+	router.HandleFunc("/settings", SettingsShowPUT).Methods("PUT")
+	router.HandleFunc("/settings", SettingsShowOPTIONS).Methods("OPTIONS")
 
 	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
 	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ORIGIN_ALLOWED")})
@@ -51,4 +56,39 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	todoId := vars["todoId"]
 	fmt.Fprintln(w, "Todo show:", todoId)
+}
+
+func SettingsShowGET(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["Test123"]
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	fmt.Fprintln(w, "Available settings:", todoId)
+}
+
+func SettingsShowPUT(w http.ResponseWriter, r *http.Request) {
+	//vars := mux.Vars(r)
+	//newValue := vars["gain"]
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	//dec := json.NewDecoder(r.Body)
+	fmt.Fprintln(w, "Set new value:", bytes.NewBuffer(body))
+}
+
+// Handler for pre-flight, browser checks for available methods before sending real request
+func SettingsShowOPTIONS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET,POST, PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	fmt.Fprintln(w, r)
 }
---
+++ axiom_beta_control_daemon/TestGUI/main.js
@@ -0,0 +1,79 @@
+function startUp() {
+    $('#demo').text("Test123");
+    $("#slider").slider();
+}
+
+function decrementISO() {
+    $.ajax({
+        url: "http://rest-service.guides.spring.io/greeting"
+    }).then(function (data) {
+        //$('.greeting-id').append(data.id);
+        //$('.greeting-content').append(data.content);
+        console.log(data.content);
+    });
+
+}
+
+function incrementISO() {
+    var JSONObject = {
+        "uname": "testUname",
+        "password": "testPassword"
+    };
+    //var jsonData = JSON.parse(JSONObject);
+
+    $.ajax({
+        url: "https://jsonplaceholder.typicode.com/posts/1",
+        type: "PUT",
+        data: JSONObject,
+        dataType: "json"
+    }).then(function (data) {
+        //$('.greeting-id').append(data.id);
+        //$('.greeting-content').append(data.content);
+        console.log(data.content);
+    });
+}
+
+function sendSettings(settingName, value) {
+    var setName = settingName;
+    var JSONObject = {
+        id : settingName,
+        value: value
+    };
+
+    $.ajax({
+        url: "http://127.0.0.1:7070/settings",
+        type: "PUT",
+        data: JSONObject,
+        crossDomain: true,
+        dataType: "text",
+        success: function (responseData, textStatus, jqXHR) {
+            console.log(responseData);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            if (xhr.status == 404) {
+                console.log(thrownError);
+            }
+        }
+    });
+}
+
+$(document).on("change", '#gain', function () {
+    var value = $('#gain').val();
+    var valueText = value;
+
+    if (value == 5) {
+        valueText = "3/3";
+    }
+
+    $('#gainValue').text(valueText);
+    sendSettings("gain", value);
+});
+
+function testFunc() {
+    console.log($('#inc_gain').data("name"));
+}
+
+// Executes when page is loaded
+$(document).ready(function () {
+    startUp();
+});
