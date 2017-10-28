47f70d537ba242202e21cb91b27d440782bd6d69
Jaro <jarohabiger@googlemail.com>
2017-10-28 18:19:26 +0200

    improve build script, add missing dependencies

--- build_tools/build_image_ubuntu.sh
+++ build_tools/build_image_ubuntu.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
+echo "starting the build..." | boxes -d parchment
+mkdir -p build
+cd build
 
-# BRANCH=xilinx-v2017.1
-BRANCH=xilinx-v2016.4
 
+echo "create the base image structure" | boxes -d parchment
 BLKOFFS=$[ 8 ]
 BLKSIZE=$[ 8*1024*1024*2 ]
 BLKBOOT=$[ 50*1024*2 ]
 BLKROOT=$[ 4*1024*1024*2 ]
-
-ARCH=arm
-CROSS=arm-linux-gnueabi- #arm-linux-gnu-
 
 BLKSEEK=$[ BLKSIZE - 1 ]
 BLKPART=$[ BLKSIZE - BLKBOOT - BLKROOT - BLKOFFS ]
 
 BLKOFF2=$[ BLKOFFS + BLKBOOT ]
 BLKOFF3=$[ BLKOFF2 + BLKROOT ]
-
 
 rm -f IMAGE.dd
 dd if=/dev/zero of=IMAGE.dd bs=512 seek=$BLKSEEK count=1
@@ -28,40 +26,34 @@ sfdisk -uS IMAGE.dd << EOF
  part4 : start=        0, size=        0, Id= 0
 EOF
 
+
+echo "Download archlinuxarm & extract it as rootfs" | boxes -d parchment
 mkdir -p ROOT.fs
-
 wget -c --quiet http://archlinuxarm.org/os/ArchLinuxARM-zedboard-latest.tar.gz
-
 tar --warning=no-unknown-keyword -x -C ROOT.fs -f ArchLinuxARM-zedboard-latest.tar.gz
 
 
-git clone --branch $BRANCH --depth 1 https://github.com/Xilinx/linux-xlnx.git linux-xlnx.git 
+echo "download and build the xillinx linux kernel; install the kernel modules into the rootfs" | boxes -d parchment
+BRANCH=xilinx-v2016.4
+git clone --branch $BRANCH --depth 1 https://github.com/Xilinx/linux-xlnx.git linux-xlnx.git
 
-git clone  --branch  https://github.com/Xilinx/linux-xlnx.git linux-xlnx.git
-#(cd linux-xlnx.git; wget http://vserver.13thfloor.at/Stuff/AXIOM/BETA/kernel-4.6.0-xilinx-00016-gb49271f.config)
-#(cd linux-xlnx.git; mv kernel-4.6.0-xilinx-00016-gb49271f.config .config)
-#(cd linux-xlnx.git; make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- oldconfig)
-#(cd linux-xlnx.git; make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-)
-#(cd linux-xlnx.git; INSTALL_MOD_PATH=../ROOT.fs make modules_install ARCH=arm)
-
+ARCH=arm
+CROSS=arm-linux-gnueabi-
 (cd linux-xlnx.git; git checkout tags/xilinx-v2016.4 -b xilinx-v2016.4 )
-
 (cd linux-xlnx.git; make CROSS_COMPILE=$CROSS ARCH=$ARCH xilinx_zynq_defconfig )
-
 (cd linux-xlnx.git; make CROSS_COMPILE=$CROSS ARCH=$ARCH -j99 )
-
 (cd linux-xlnx.git; make CROSS_COMPILE=$CROSS ARCH=$ARCH zynq-zed.dtb )
-
 (cd linux-xlnx.git; make CROSS_COMPILE=$CROSS ARCH=$ARCH -j99 modules)
-
 (cd linux-xlnx.git; make CROSS_COMPILE=$CROSS ARCH=$ARCH INSTALL_MOD_PATH=../ROOT.fs modules_install )
 
 
+echo "create the bootfs" | boxes -d parchment
 mkdir -p BOOT.fs
-
 cp -va linux-xlnx.git/arch/arm/boot/zImage BOOT.fs/
 cp -va linux-xlnx.git/arch/arm/boot/dts/zynq-zed.dtb BOOT.fs/devicetree.dtb
 
+
+echo "create the boot partition & assamble it into the image" | boxes -d parchment
 rm -f BOOT.part
 dd if=/dev/zero of=BOOT.part bs=512 seek=$[ BLKBOOT - 1] count=1
 mkfs.vfat -n "BOOT" -F 32 BOOT.part $[ BLKBOOT / 2 ]
@@ -69,19 +61,22 @@ mcopy -i BOOT.part BOOT.fs/* ::
 
 dd if=BOOT.part of=IMAGE.dd bs=512 seek=$BLKOFFS count=$BLKBOOT conv=sparse,notrunc
 
+
+echo "create the root partition & assamble it into the image" | boxes -d parchment
 rm -f ROOT.part
 dd if=/dev/zero of=ROOT.part bs=512 seek=$[ BLKROOT - 1] count=1
-
 mkfs.ext4 -d ROOT.fs ROOT.part $[ BLKROOT / 2 ]
 
 dd if=ROOT.part of=IMAGE.dd bs=512 seek=$BLKOFF2 count=$BLKROOT conv=sparse,notrunc
 
+
+echo "download & buid qemu" | boxes -d parchment
 git clone https://github.com/Xilinx/qemu.git qemu-xlnx.git
 
 (cd qemu-xlnx.git; git submodule update --init pixman dtc )
 
 (cd qemu-xlnx.git; ./configure --target-list="aarch64-softmmu" --enable-fdt --disable-kvm --disable-xen )
-(cd qemu-xlnx.git; make -j99 )
+(cd qemu-xlnx.git; make -j$(nproc) )
 
-
+# to run qemu use the following command:
 # qemu-xlnx.git/aarch64-softmmu/qemu-system-aarch64 -M arm-generic-fdt-7series -machine linux=on -serial /dev/null -serial mon:stdio -nographic -dtb BOOT.fs/devicetree.dtb -kernel BOOT.fs/zImage -drive if=sd,format=raw,index=0,file=IMAGE.dd -boot mode=5 -append "root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4" 
