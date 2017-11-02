15b9d2fe6e7bea535a8cbc2639984886dfa275b1
BAndiT1983 <b.andrej@cumir.de>
2017-11-02 18:33:51 +0100

    - Adjusted daemon install script to place daemon and related files in some global place, currently /opt/axiom_daemon - Disabled building examples and tests in Pistache, modified CMakeLists.txt

--- software/control_daemon/systemd/install_daemon.sh
+++ software/control_daemon/systemd/install_daemon.sh
@@ -1,8 +1,33 @@
-mkdir /opt/axiom_daemon
+echo "--- Check sudo";
+if [[ $EUID != 0 ]];
+then
+    echo "FAILED: Please run as sudo";
+    exit;
+fi
+
+echo "--- Create folder for daemon";
+if [ ! -d "/opt/axiom_daemon" ]; then
+    mkdir /opt/axiom_daemon
+fi
+
+# Just in case the folder was moved
+echo "--- Remove old links of systemd description files";
+rm /etc/systemd/system/axiom.service
+rm /etc/systemd/system/axiom.socket
+
+echo "--- Copy daemon and systemd description files to /opt";
 cp -f axiom.service /opt/axiom_daemon/
 cp -f axiom.socket /opt/axiom_daemon/
+cp -f axiom_daemon /opt/axiom_daemon/
+
+echo "--- Make new links for systemd description files";
 ln -s /opt/axiom_daemon/axiom.service /etc/systemd/system
 ln -s /opt/axiom_daemon/axiom.socket /etc/systemd/system
+
+echo "--- Reload daemon info";
+systemctl daemon-reload
+
+echo "--- Enable axiom service and show status";
 systemctl enable axiom
 systemctl start axiom
 systemctl status axiom
