a5ff34b654535cab927c7636068a3f4d7f9e4a49
Sebastian Pichelhofer <sp@apertus.org>
2018-10-28 16:09:16 +0100

    added 2 missing scripts

---
+++ software/scripts/hdmi_init2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+EMIOBASE=960
+
+for n in `seq 0 63`; do 
+    N=$[ EMIOBASE + n ]
+    gpio=/sys/class/gpio/gpio$N
+    [ -e $gpio ] ||
+	echo $N >/sys/class/gpio/export
+    echo out >$gpio/direction
+    echo 0 >$gpio/value
+done
+
+echo 1 >/sys/class/gpio/gpio974/value 
+echo 1 >/sys/class/gpio/gpio973/value 
+echo 1 >/sys/class/gpio/gpio972/value 
+
+echo 0 >/sys/class/gpio/gpio974/value 
+
+# i2cset -y 2 0x70 0x0 0x5	# RFW
+# sleep 0.5
+# i2cdetect -r -y -a 2
+
