cc868e25f449b56e17b2b92bbd03d2fe5eb3d8f4
Robin Heinemann <robin.ole.heinemann@t-online.de>
2019-03-10 21:06:28 +0100

    prepare for qemu patches

--- makefiles/host/qemu.mk
+++ makefiles/host/qemu.mk
@@ -3,10 +3,15 @@ QEMU_SOURCE = build/qemu-$(QEMU_VERSION).git
 
 QEMU_MAKE = $(MAKE) -C $(QEMU_SOURCE)
 
-$(QEMU_SOURCE):
+
+QEMU_PATCHES = $(wildcard patches/qemu/*.patch)
+
+$(QEMU_SOURCE): $(QEMU_PATCHES)
 	@mkdir -p $(@D)
+	rm -rf $@
 	git clone --branch $(QEMU_VERSION) --depth 1 https://github.com/Xilinx/qemu.git $@
 	(cd $(QEMU_SOURCE) && git submodule update --init dtc)
+	./makefiles/host/patch_wrapper.sh $@ $(QEMU_PATCHES)
 
 $(QEMU_SOURCE)/aarch64-softmmu/qemu-system-aarch64: $(QEMU_SOURCE)
 	# disable werror due to upstream bugs
