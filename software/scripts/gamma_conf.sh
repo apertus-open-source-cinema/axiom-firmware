a0fb7d03f88961cc101f83703ee3a6ff4720bc71
Sebastian Pichelhofer <sp@apertus.org>
2019-04-04 09:39:28 +0200

    fixed lutconf bug
    https://github.com/apertus-open-source-cinema/axiom-beta-firmware/issues/94

--- software/scripts/gamma_conf.sh
+++ software/scripts/gamma_conf.sh
@@ -4,7 +4,7 @@ cd $(dirname $(realpath $0))    # change into script dir
 MAX=65536
 GAMMA=${1:-1.0}
 
-../processing_tools/lut_conf3/lut_conf3 -N 4096 -M $MAX -G $GAMMA -B 0x80300000
-../processing_tools/lut_conf3/lut_conf3 -N 4096 -M $MAX -G $GAMMA -B 0x80304000
-../processing_tools/lut_conf3/lut_conf3 -N 4096 -M $MAX -G $GAMMA -B 0x80308000
-../processing_tools/lut_conf3/lut_conf3 -N 4096 -M $MAX -G $GAMMA -B 0x8030C000
+../processing_tools/lut_conf/lut_conf -N 4096 -M $MAX -G $GAMMA -B 0x80300000
+../processing_tools/lut_conf/lut_conf -N 4096 -M $MAX -G $GAMMA -B 0x80304000
+../processing_tools/lut_conf/lut_conf -N 4096 -M $MAX -G $GAMMA -B 0x80308000
+../processing_tools/lut_conf/lut_conf -N 4096 -M $MAX -G $GAMMA -B 0x8030C000
