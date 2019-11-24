64c2e7b47bdebe0e329d47ccb77b8fcf34e5314e
Jaro Habiger <jarohabiger@googlemail.com>
2019-11-24 14:21:46 +0100

    [software] incooperate ctrl and webui

--- makefiles/host/docker_entry.sh
+++ makefiles/host/docker_entry.sh
@@ -2,7 +2,9 @@
 # this script is called from the docker-make.sh script and sets up everything in the 
 # docker container & executes make
 
-stty cols $COLUMNS rows $LINES;
+if [ -z $COLUMNS ] && [ -z $LINES ]; then
+    stty cols "$COLUMNS" rows "$LINES";
+fi
 
 # this is a stupid hack to work around this bug: https://bugs.launchpad.net/qemu/+bug/1805913
 mkdir -p build/root.fs
@@ -10,16 +12,16 @@ if [ ! -z $CI ]; then
     mount -t tmpfs tmpfs build/root.fs
     echo "WARNING: building rootfs in ramdisk; incremental builds suffer"
 else
-    if [ ! -f build/root.fs.loopback.img ]; then
+    if [ ! -f 'build/root.fs.loopback.img' ]; then
         truncate -s 4G build/root.fs.loopback.img
         mkfs.btrfs build/root.fs.loopback.img
     fi
-    losetup -d /dev/loop0
+    losetup -d /dev/loop0 2> /dev/null
     losetup /dev/loop0 build/root.fs.loopback.img
     mount /dev/loop0 build/root.fs
 fi
 
-make -f makefiles/host/main.mk -I makefiles/host -j $(nproc) $*
+make -f makefiles/host/main.mk -I makefiles/host -j "$(nproc)" $*
 RETURN_CODE=$?
 
 umount build/root.fs
--- software/http/libraries/registers.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-. ./cmv.func
-
-for n in `seq 0 4 511`; do devmem $(( 0x60000000 + n )) w; done
