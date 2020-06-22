2abfb84ab464168681a32c95ec42662e1983184b
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2020-06-22 19:05:36 +0200

    makefiles / software: finish - -> _ rename

--- software/scripts/rcn_validation.sh
+++ software/scripts/rcn_validation.sh
@@ -4,11 +4,11 @@ cd $(dirname $(realpath $0))    # change into script dir
 
 export BETA=192.168.1.101 # replace by your camera IP
 
-ssh root@$BETA "axiom-rcn-clear"
-ssh root@$BETA "axiom-snap -2 -b -r -e 10ms" > dark-check-1.raw12 
+ssh root@$BETA "axiom_rcn-clear"
+ssh root@$BETA "axiom_snap -2 -b -r -e 10ms" > dark-check-1.raw12
 
-ssh root@$BETA "axiom-rcn-darkframe darkframe-x1.pgm"
-ssh root@$BETA "axiom-snap -2 -b -r -e 10ms" > dark-check-2.raw12 
+ssh root@$BETA "axiom_rcn_darkframe darkframe-x1.pgm"
+ssh root@$BETA "axiom_snap -2 -b -r -e 10ms" > dark-check-2.raw12
 
 ./raw2dng --no-darkframe --check-darkframe dark-check-1.raw12
 ./raw2dng --no-darkframe --check-darkframe dark-check-2.raw12
