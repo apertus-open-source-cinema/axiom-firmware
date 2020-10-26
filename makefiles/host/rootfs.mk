aabedc6069bf95c942f2876a8fa28c4bd43869d9
anuejn <jarohabiger@googlemail.com>
2020-10-26 16:23:14 +0100

    [os] install openocd (#174)
    Co-authored-by: Robin Heinemann <robin.ole.heinemann@t-online.de>

--- makefiles/host/rootfs.mk
+++ makefiles/host/rootfs.mk
@@ -2,7 +2,10 @@ include bootfs.mk
 
 LINUX_BASE_IMAGE=ArchLinuxARM-zedboard-latest.tar.gz
 
-build/root.fs/.install_stamp: $(shell find makefiles/in_chroot/) build/root.fs/opt/axiom-firmware/.install_stamp $(LINUX_SOURCE)/arch/arm/boot/zImage build/root.fs/.base_install build/webui/dist/index.html build/nctrl/target/release/nctrl
+OPENOCD_VERSION = d46f28c2ea2611f5fbbc679a5eed253d3dcd2fe3
+OPENOCD_SOURCE = build/openocd-$(OPENOCD_VERSION).git
+
+build/root.fs/.install_stamp: $(shell find makefiles/in_chroot/) build/root.fs/opt/axiom-firmware/.install_stamp $(LINUX_SOURCE)/arch/arm/boot/zImage build/root.fs/.base_install build/webui/dist/index.html build/nctrl/target/release/nctrl $(OPENOCD_SOURCE)/.build_stamp
 	rsync -aK build/kernel_modules.fs/ $(@D)
 
 	cp -r build/webui/dist $(@D)/opt/axiom-firmware/software/webui
@@ -56,3 +59,17 @@ build/nctrl/target/release/nctrl: build/nctrl/.copy_stamp
 	FUSE_CROSS_STATIC_PATH=./thirdparty/ \
 	FUSE_CROSS_STATIC_LIB=fuse \
 	cargo build --release --target=armv7-unknown-linux-musleabihf
+
+$(OPENOCD_SOURCE): build/root.fs/.base_install
+	@mkdir -p $(@D)
+	rm -rf $@
+	git clone https://repo.or.cz/openocd.git $@
+	(cd $@ && git reset --hard $(OPENOCD_VERSION))
+	touch $@/.scmversion
+
+$(OPENOCD_SOURCE)/.build_stamp: $(OPENOCD_SOURCE) build/root.fs/.base_install
+	(cd $(@D) && ./bootstrap)
+	(cd $(@D) && PKG_CONFIG=/root/armv7-eabihf--musl--bleeding-edge-2020.02-2/bin/pkg-config ./configure --host=arm-buildroot-linux-musleabihf --enable-static --enable-sysfsgpio --prefix=$${PWD}/../root.fs/usr/ CFLAGS="--static")
+	+(cd $(@D) &&  $(MAKE))
+	+(cd $(@D) &&  $(MAKE) install)
+	touch $@
