21f7c5ecf844101a82b05c66a153700a56db80df
Robin Ole Heinemann <robin.ole.heinemann@gmail.com>
2021-11-14 18:29:32 +0000

    [scripts] add support for raw mode

---
+++ software/scripts/axiom_gen_init_hdmi.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+case $1 in
+  1080p60|1080p30)
+    #   Total	Active	Front	Sync	Back
+    H=(	2200	1920	  88	  44	 148 )
+    V=( 1125	1080	   4	   5	  36 )
+    D=(   32 )
+    ;;
+
+  1080p50|1080p25)
+    #   Total	Active	Front	Sync	Back
+    H=(	2640	1920	 528	  44	 148 )
+    V=( 1125	1080	   4	   5	  36 )
+    D=(   32 )
+    ;;
+
+  1080p48|1080p24)
+    #   Total	Active	Front	Sync	Back
+    H=(	2750	1920	 638	  44	 148 )
+    V=( 1125	1080	   4	   5	  36 )
+    D=(   32 )
+    ;;
+
+  2048x1080p60)
+    #   Total	Active	Front	Sync	Back
+    H=(	2200	2048	  56	  44	  52 )
+    V=( 1125	1080	   4	   5	  36 )
+    D=(   32 )
+    ;;
+	
+  2048x1080p50)
+    #   Total	Active	Front	Sync	Back
+    H=(	2640	2048	 400	  44	 148 )
+    V=( 1125	1080	   4	   5	  36 )
+    D=(   32 )
+    ;;
+esac
+
+
+# Sync + Back Porch
+HAS=$[ ${H[3]} + ${H[4]} ]
+VAS=$[ ${V[3]} + ${V[4]} ]
+
+# Active Start + Active
+HAE=$[ $HAS + ${H[1]} ]
+VAE=$[ $VAS + ${V[1]} ]
+
+# Sync starts at 0
+HSS=0
+VSS=0
+
+# Sync ends after Sync
+HSE=${H[3]}
+VSE=${V[3]}
+
+# Guard is 2 Pixel wide
+GRD=$[ $HAS - 2 ]
+
+# Preamble is 8 Pixel
+PRE=$[ $GRD - 8 ]
+
+# Data Islands 
+TRC=$[ $HAS + ${D[0]} ]
+
+# Guard is 2 Pixel after DI
+GDE=$[ $TRC + 2 ]
+
+# Active End + Front Porch 
+HT=$[ $HAE + ${H[2]} ]
+VT=$[ $VAE + ${V[2]} ]
+
+
+[ $HT -eq ${H[0]} ] || {
+    echo "mismatch horizontal total $HT not equal ${H[0]}"
+    exit 1
+}
+
+[ $VT -eq ${V[0]} ] || {
+    echo "mismatch vertical total $VT not equal ${V[0]}"
+    exit 1
+}
+
+[ $PRE -gt $HSE ] || {
+    echo "preamble $PRE overlaps horizontal sync $HSE"
+    exit 1
+}
+
+
+axiom_scn_reg  0 $HT		# total_w
+axiom_scn_reg  1 $VT		# total_h
+axiom_scn_reg  2 256		# total_f
+
+axiom_scn_reg  4 $HAS		# hdisp_s
+axiom_scn_reg  5 $HAE		# hdisp_e
+axiom_scn_reg  6 $VAS		# vdisp_s
+axiom_scn_reg  7 $VAE		# vdisp_e
+
+axiom_scn_reg  8 $HSS		# hsync_s
+axiom_scn_reg  9 $HSE		# hsync_e
+axiom_scn_reg 10 $VSS		# vsync_s
+axiom_scn_reg 11 $VSE		# vsync_e
+
+axiom_scn_reg 32 $PRE		# pream_s
+axiom_scn_reg 33 $GRD		# guard_s
+axiom_scn_reg 34 $TRC		# terc4_e
+axiom_scn_reg 35 $GDE		# guard_e
+
--- software/scripts/axiom_setup.sh
+++ software/scripts/axiom_setup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+
+MODE=${1:-normal}
+
 # devmem 0x80010018 32 0x00000
 
 # axiom_gen_init.sh 1080p60
 # axiom_gen_init.sh 1080p50
 # axiom_gen_init.sh 1080p30
 # axiom_gen_init.sh 1080p24
-axiom_gen_init.sh SHOGUN
+
+[ "$MODE" == "normal" ] && axiom_gen_init.sh SHOGUN
+[ "$MODE" == "raw" ] && axiom_gen_init.sh 1080p60
 
 axiom_data_init.sh
 axiom_rmem_conf.sh
 axiom_wmem_conf.sh
 # axiom_linear_conf.sh 1.3 0.0
-axiom_linear_conf.sh 1.0 0.0
+[ "$MODE" == "normal" ] && axiom_linear_conf.sh 1.0 0.0
 # axiom_linear_conf.sh 1.6 0.0
 # ./remap_conf.sh $DISP
 # axiom_gamma_conf.sh 1
@@ -22,6 +27,8 @@ axiom_gamma_conf.sh 0.8
 # axiom_mat4_conf.sh 0.3 0.3 0.3 0.3  0 0 0 1  0 0.5 0.5 0  1 0 0 0
 axiom_mat4_conf.sh 0.3 0.3 0.3 0.3  0 0 0 1  0 0.42 0.42 0  1 0 0 0
 
-axiom_scn_reg 31 3
-axiom_scn_reg 31 1
+[ "$MODE" == "normal" ] && axiom_scn_reg 31 3
+[ "$MODE" == "normal" ] && axiom_scn_reg 31 1
 
+[ "$MODE" == "raw" ] && axiom_scn_reg 31 0x4000
+[ "$MODE" == "raw" ] && axiom_scn_reg 30 0x4000
